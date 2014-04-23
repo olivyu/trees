878ffe13b9ef46883375775b66470663d34d8357
Olivia <olivyu@gmail.com>
2014-04-23 09:52:36 -0700

    tree files

---
+++ lib/ExistenceException.java
@@ -0,0 +1,7 @@
+package trees;
+
+public class ExistenceException extends Exception {
+	public ExistenceException(String message){
+		super(message);
+	}
+}
---
+++ lib/Tester.java
@@ -0,0 +1,41 @@
+package trees;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tester {
+	
+	public static void main(String[] args) throws ExistenceException{
+		Tree my_tree = new Tree();
+		
+		Random random = new Random();
+		int[] storage = new int[20];
+		for (int j = 0; j < storage.length; j++){
+			int next = random.nextInt(100);
+			storage[j] = next;
+			my_tree.insert(next);
+		}
+		
+		System.out.println(Arrays.toString(storage));
+		my_tree.balance();
+		my_tree.display();
+		System.out.println(my_tree.count());
+
+		for (int i : storage){
+			System.out.println(i);
+			System.out.println(my_tree.search(i));
+			my_tree.delete(i);
+			//System.out.println(my_tree.count());
+			my_tree.display();
+		}
+		
+		System.out.println(my_tree.count());
+		
+		//my_tree.display();
+		
+		//System.out.println(my_tree.height());
+		
+	}
+}
+
